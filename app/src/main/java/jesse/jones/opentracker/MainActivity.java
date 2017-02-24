db6f080927522aa76a68bc429975746950fe5c21
Jesse <jjonesdesign@gmail.com>
2017-02-24 12:53:51 -0800

    Made GPS stop updating after getting location. Updated location now clears old marker. Added button to get current location again.

--- app/src/main/java/jesse/jones/opentracker/MainActivity.java
+++ app/src/main/java/jesse/jones/opentracker/MainActivity.java
@@ -4,10 +4,13 @@ import android.Manifest;
 import android.content.Context;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -18,8 +21,11 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import jesse.jones.opentracker.network.GooglePlacesService;
 import jesse.jones.opentracker.network.entity.GetGooglePlacesResponse;
 import jesse.jones.opentracker.network.entity.Location;
@@ -29,8 +35,10 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener{
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener,GoogleMap.OnMapClickListener{
 
+    @BindView(R.id.updateLocationButton)
+    FloatingActionButton mUpdateLocationButton;
 
 
     GoogleMap mMap;
@@ -39,6 +47,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     GooglePlacesService mPlacesService;
 
     LatLng mLocation;
+    LatLng mPreviousLocation;
+    ArrayList<LatLng> mLocationArray;
     LocationManager mLocationManager;
 
     Boolean mMapReady = false;
@@ -78,21 +88,19 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         });
 
-        int permissionCheck = ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION);
-        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == 0){
+            mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        }
+
+
     }
 
     @Override
     public void onMapReady(GoogleMap map) {
         mMap = map;
-
-        if(mLocation != null){
-            mMap.addMarker(new MarkerOptions().position(mLocation).title("Marker in Sydney"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(mLocation));
-        }
-
+        mMap.setOnMapClickListener(this);
         mMapReady = true;
 
 
@@ -115,8 +123,21 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         //txtLat.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
         mLocation = new LatLng(location.getLatitude(),location.getLongitude());
         if(mMapReady){
-            mMap.addMarker(new MarkerOptions().position(mLocation).title("Marker in Sydney"));
+
+            Toast.makeText(MainActivity.this, "Location Updated", Toast.LENGTH_LONG).show();
+            mMap.clear();
+            mMap.addMarker(new MarkerOptions().position(mLocation).title("My Location"));
+            mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
             mMap.moveCamera(CameraUpdateFactory.newLatLng(mLocation));
+
+
+            mPreviousLocation = mLocation;
+            if(mLocationManager != null) {
+                if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == 0){
+                    mLocationManager.removeUpdates(this);
+                }
+            }
+
         }
 
 
@@ -125,5 +146,46 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
         //Log.d("Latitude","status");
+    }
+
+    /** calculates the distance between two locations in MILES */
+    private double distance(double lat1, double lng1, double lat2, double lng2) {
+
+        double earthRadius = 3958.75; // in miles, change to 6371 for kilometer output
+
+        double dLat = Math.toRadians(lat2-lat1);
+        double dLng = Math.toRadians(lng2-lng1);
+
+        double sindLat = Math.sin(dLat / 2);
+        double sindLng = Math.sin(dLng / 2);
+
+        double a = Math.pow(sindLat, 2) + Math.pow(sindLng, 2)
+                * Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2));
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+        double dist = earthRadius * c;
+
+        return dist; // output distance, in MILES
+    }
+
+
+
+    //Click Handlers
+    @OnClick(R.id.updateLocationButton)
+    public void updateLocationButtonClicked(FloatingActionButton button) {
+        Toast.makeText(MainActivity.this, "Updating Location", Toast.LENGTH_SHORT).show();
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == 0){
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        }
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        Toast.makeText(MainActivity.this, "Location Set Manually", Toast.LENGTH_SHORT).show();
+        mLocation = latLng;
+        mMap.clear();
+        mMap.addMarker(new MarkerOptions().position(mLocation).title("My Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(mLocation));
     }
 }
