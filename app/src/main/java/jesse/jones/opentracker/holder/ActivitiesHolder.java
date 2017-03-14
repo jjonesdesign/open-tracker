00fd6a54cf1be01b2f13897a41dddfdae879f241
Jesse <jjonesdesign@gmail.com>
2017-03-14 12:43:48 -0700

    Fixed list stlying

--- app/src/main/java/jesse/jones/opentracker/holder/ActivitiesHolder.java
+++ app/src/main/java/jesse/jones/opentracker/holder/ActivitiesHolder.java
@@ -12,18 +12,22 @@ import jesse.jones.opentracker.R;
 
 public class ActivitiesHolder extends RecyclerView.ViewHolder {
     TextView mName;
+    TextView mDescription;
 
     public ActivitiesHolder(View itemView) {
         super(itemView);
         mName = (TextView) itemView.findViewById(R.id.activityName);
+        mDescription = (TextView) itemView.findViewById(R.id.activityDescription);
     }
 
     public void setName(String name) {
         mName.setText(name);
     }
+    public void setDescripion(String descripion) { mDescription.setText(descripion); }
 
 
     public void clear() {
         mName.setText("");
+        mDescription.setText("");
     }
 }
